b7ae15d73f4efb968ddc4868ee8ba6656cd2e2a2
Yash <yashr3712@gmail.com>
2026-05-25 20:34:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+const promptInput =
+document.getElementById("prompt")
+
+const sendBtn =
+document.getElementById("send-btn")
+
+const chatArea =
+document.getElementById("chat-area")
+
+function addMessage(text, className){
+
+    const div =
+    document.createElement("div")
+
+    div.classList.add(className)
+
+    div.innerText = text
+
+    chatArea.appendChild(div)
+
+    chatArea.scrollTop =
+    chatArea.scrollHeight
+}
+
+async function sendMessage(){
+
+    const text =
+    promptInput.value.trim()
+
+    if(text === "") return
+
+    addMessage(text, "user-message")
+
+    promptInput.value = ""
+
+    addMessage(
+        "Thinking...",
+        "jarvis-message"
+    )
+
+    const thinkingDiv =
+    chatArea.lastChild
+
+    try{
+
+        const response =
+        await fetch(
+            "http://127.0.0.1:5000/chat",
+            {
+                method:"POST",
+
+                headers:{
+                    "Content-Type":
+                    "application/json"
+                },
+
+                body:JSON.stringify({
+                    message:text
+                })
+            }
+        )
+
+        const data =
+        await response.json()
+
+        thinkingDiv.innerText =
+        data.reply
+
+    }
+
+    catch(error){
+
+        thinkingDiv.innerText =
+        "Backend connection failed."
+    }
+}
+
+sendBtn.addEventListener(
+    "click",
+    sendMessage
+)
+
+promptInput.addEventListener(
+    "keydown",
+    (e)=>{
+
+        if(e.key === "Enter"){
+            sendMessage()
+        }
+    }
+)
